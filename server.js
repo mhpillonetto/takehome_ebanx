979ecfdf4936c90db3b294690bc564781f3fb5b9
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2022-11-11 12:14:28 -0300

    add event:deposit route

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ fastify.post('/reset', async (request, reply) => {
       .code(200)
       .header('Content-Type', 'application/json; charset=utf-8')
       .send('OK')
-  })
+})
 
 
 fastify.get('/balance', async (request, reply) => {    
-    const account = await request.query;
+    //placeholder
     const existing_accounts = ['1','2','3'];
+
+    const account = await request.query;
 
     existing_accounts.includes(account.account_id) ? (
         reply
@@ -27,6 +29,40 @@ fastify.get('/balance', async (request, reply) => {
         .code(404)
         .send('account not found.')
 })
+
+fastify.post('/event', async (request, reply) => {
+    //placeholder
+    const existing_accounts = [
+        {account_id:'1',balance:0},
+        {account_id:'2',balance:0},
+        {account_id:'3',balance:0}
+    ];
+    
+    const type = request.body.type;
+    const account_id = request.body.destination;
+    const amount = request.body.amount;
+
+    switch (type) {
+        case 'deposit':
+             existing_accounts.includes(account_id) ?
+                // deposit(destination, amount)
+                reply
+                    .code(201)
+                    .send({"destination":{"id":account_id,"balance":amount}}) 
+                :
+                reply
+                //createAccount(account_id)
+                //deposit(destination, amount)
+                    .code(201)
+                    .send({"destination":{"id":account_id,"balance":amount}})
+        default:
+                reply
+                    .code(201)
+                    .send({"destination":{"id":1,"balance":10}})
+    }
+   
+})
+
 
 const start = async () => {
   try {

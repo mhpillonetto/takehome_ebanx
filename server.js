38602a0b7cd17e3a92c329a70d95ccbd2aa24de7
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2022-11-11 11:22:41 -0300

    set up routes

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-async/await
-
 const fastify = require('fastify')({ logger: true })
 
-
+// routes
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
 
+fastify.post('/reset', async (request, reply) => {
+
+    reply
+      .code(200)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send('OK')
+  })
+
+
+fastify.get('/balance', async (request, reply) => {    
+    const account = await request.query;
+    const existing_accounts = ['1','2','3'];
+
+    existing_accounts.includes(account.account_id) ? (
+        reply
+            .code(200)
+            .send(account)
+    ) : 
+    reply
+        .code(404)
+        .send('account not found.')
+})
 
 const start = async () => {
   try {
@@ -16,4 +36,5 @@ const start = async () => {
     process.exit(1)
   }
 }
+
 start()

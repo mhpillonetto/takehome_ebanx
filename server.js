5a8154d54d6a03e247a75c8bd469d3c592141169
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2022-11-14 15:24:29 -0300

    add event:transfer route

--- server.js
+++ server.js
@@ -39,36 +39,45 @@ fastify.post('/event', async (request, reply) => {
     ];
     
     const type = request.body.type;
-    const account_id = request.body.destination;
+    const destination = request.body.destination;
+    const origin = request.body.origin;
     const amount = request.body.amount;
 
     switch (type) {
         case 'deposit':
-             existing_accounts.includes(account_id) ?
+            existing_accounts.includes(destination) ?
                 // deposit(destination, amount)
                 reply
                     .code(201)
-                    .send({"destination":{"id":account_id,"balance":amount}}) 
+                    .send({"destination":{"id":destination,"balance":amount}}) 
                 :
-                reply
                 //createAccount(account_id)
                 //deposit(destination, amount)
+                reply
                     .code(201)
-                    .send({"destination":{"id":account_id,"balance":amount}})
+                    .send({"destination":{"id":destination,"balance":amount}})
         
-        case 'withdrawal':
-             existing_accounts.includes(account_id) ?
-                // deposit(destination, amount)
+        case 'withdraw':
+             existing_accounts.includes(origin) ?
+                // withdraw(origin, amount)
                 reply
                     .code(201)
-                    .send({"destination":{"id":account_id,"balance":amount}}) 
+                    .send({"destination":{"id":origin,"balance":amount}}) 
                 :
                 reply
-                //createAccount(account_id)
-                //deposit(destination, amount)
                     .code(404)
-                    .send("Account not found")
+                    .send(0)
 
+        case 'transfer':
+             existing_accounts.includes(origin) && existing_accounts.includes(destination) ?
+                // transfer(origin, destination, amount)
+                reply
+                    .code(201)
+                    .send({"origin":{"id":account_id,"balance":amount}, "destination":{"id":account_id,"balance":amount}}) 
+                :
+                reply
+                    .code(404)
+                    .send(0)
         default:
                 reply
                     .code(201)
